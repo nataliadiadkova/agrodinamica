5609c62b9d21ec6bff54769727e62ee85e728a5e
nataliadiadkova <nataliadiadkova@gmail.com>
2018-02-27 20:56:56 +0200

    design-item

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
   $('.partner-slide').slick({
-    
+
+  });
+});
+
+$(document).ready(function(){
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true
   });
 });
